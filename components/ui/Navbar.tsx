de6038b356c4dcee953c3f97f91bcf95250b3c78
sachinthakur749 <sachinthakur9983@gmail.com>
2024-11-26 22:10:09 +0545

    ui-enhanced

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex  py-2 px-1 justify-around items-center">
-      <div className="w-1/3">
-        <a className="font-bold text-[30px]" href="/">
-          One<span className="text-primary font-extrabold">Post</span>
-        </a>
-      </div>
-      <div className="w-3/6">
-        <input
-          className=" bg-[#ededed] w-full rounded-[25px] pl-[15px] py-[8px] border-[#948f8f]"
-          type="search"
-          placeholder="search..."
-        />
-      </div>
-      <div className="w-1/3">
-        <button className="w-full">Search</button>
+    <div className="bg-white/85 border-b border-gray-30 sticky z-50 backdrop-blur-md top-0 py-2 ">
+      <div className="flex max-w-[1200px] mx-auto px-4  py-2  justify-around items-center">
+        <div className="w-1/3">
+          <a className="font-bold text-[30px]" href="/">
+            One<span className="text-primary font-extrabold">Post</span>
+          </a>
+        </div>
+        <div className="w-3/6">
+          <input
+            className=" bg-[#ededed] w-full rounded-[25px] pl-[15px] py-[8px] border-[#948f8f]"
+            type="search"
+            placeholder="search..."
+          />
+        </div>
+        <div className="w-1/3">
+          <button className="w-full">Search</button>
+        </div>
       </div>
     </div>
   );
--- components/ui/sections/BannerSection.tsx
+++ components/ui/sections/BannerSection.tsx
@@ -3,18 +3,29 @@ import React from "react";
 
 const BannerSection = () => {
   return (
-    <div className="mt-5">
-      <h1 className="text-[35px] font-semibold ">
-        one post, <span className="text-gray-500">Endless Insights</span>{" "}
+    <div className="mt-[100px]">
+      <h1 className="text-[40px]   w-[80%] font-extrabold">
+        Discover the latest trends and untold stories.
+        <span className="font-semibold text-gray-800">
+          Stay ahead with insights that matter.
+        </span>
       </h1>
-      <div>
+      <div className="relative mt-5">
         <Image
           alt="logo"
-          src="/banner-image.jpg"
+          src="/banner-image-1.jpg"
           width={500}
           height={500}
-          className="w-full h-[450px] rounded-[10px] object-cover "
+          className="w-full h-[450px] rounded-[10px] object-cover"
         />
+        <div
+          className="absolute flex justify-center items-center inset-0 rounded-[10px] bg-gradient-to-t from-black/60 to-transparent"
+          style={{ zIndex: 1 }}
+        >
+          {/* <h1 className="text-[45px]  font-semibold">
+            one post, <span className="text-black ">Endless Insights</span>
+          </h1> */}
+        </div>
       </div>
     </div>
   );
